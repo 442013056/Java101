a047bec6a57d05ad8d5bed224b2bdb188e4d3ba1
AbdullahAlhassani <91234749+AbdullahAlhassani@users.noreply.github.com>
2021-09-25 20:41:13 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Enter two float digits :");
+        float num1 = numbers.nextFloat();
+        float num2 = numbers.nextFloat();
+        System.out.println("Addition + :"+(num1+num2));
+        System.out.println("Subtraction  - :"+(num1-num2));
+        System.out.println("Multiplication  * :"+(num1*num2));
+        System.out.println("Division / :"+(num1/num2));
+        System.out.println("Modulus % :"+(num1%num2));
+        System.out.println("Grater > :"+(num1>num2));
+        System.out.println("Grater or equal :"+(num1>=num2));
+        System.out.println("Less :"+(num1<num2));
+        System.out.println("Less or equal :"+(num1<=num2));
+        System.out.println("Not equal :"+(num1!=num2));
+        System.out.println("Equal :"+(num1==num2));
+        //عبدالله فيصل الحساني
+        //442013056
+    }
+}
