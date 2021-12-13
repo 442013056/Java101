e65ddf669abffdd02c916e42165c138e27423f6b
442013056 <91234749+442013056@users.noreply.github.com>
2021-12-13 21:51:42 +0300

    Create radius.java

---
+++ radius.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+  
+    System.out.println("Entar a Radius of circle ");   
+    
+    float var = scan.nextFloat();
+    
+    System.out.println(3.14 * var * var);
+   
+    }
+    
+}
