8c2c0398444a2dfabd8002cac96e61820d6657c1
442013056 <91234749+442013056@users.noreply.github.com>
2021-12-15 22:48:35 +0300

    Create readAndrepllace.java

---
+++ readAndrepllace.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+    public static void main (String[] args) {
+     
+     String name = "abdollahFasail";
+     for (int N =0; N<=13; ++N){
+         if(name.charAt(N) == 'l'){
+             System.out.println('_');}
+             else{
+                 System.out.println(name.charAt(N));
+             }
+     }
+     
+     
+    }
+}
