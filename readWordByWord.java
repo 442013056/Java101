ca547174b9f58715a4f333f2ac1ac0f6822aa906
442013056 <91234749+442013056@users.noreply.github.com>
2021-12-15 23:16:13 +0300

    Create readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    
+    public static void main(String[] args) {
+            
+            
+            
+       String var = "Abdollah\nalhassani";
+
+        System.out.println(var);
+
+
+
+        }
+}
