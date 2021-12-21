f044c58f4c39e04dda92da5a91330ced2c7c6035
442013056 <91234749+442013056@users.noreply.github.com>
2021-12-21 10:27:45 +0300

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("a number of 100 to -100");
+		int counter=100;
+		while(counter>=-100){
+		    if(counter%2==0){
+		        System.out.println("("+counter+")"+"%2 is even");}
+		 else{
+		     System.out.println("("+counter+")"+"%2 is odd"); }
+		    counter--;
+		    
+		    
+		}
+	}
+}
